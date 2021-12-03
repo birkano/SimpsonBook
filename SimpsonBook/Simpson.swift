93c4215976e7ed4fbd9e37c5c0a41ae9a3c68e26
Birkan <birkanpusa@icloud.com>
2021-12-03 17:27:32 +0300

    first commit

---
+++ SimpsonBook/Simpson.swift
@@ -0,0 +1,8 @@
+//
+//  Simpson.swift
+//  SimpsonBook
+//
+//  Created by Birkan Pusa on 3.12.2021.
+//
+
+import Foundation
---
+++ SimpsonBook/detailsViewController.swift
@@ -0,0 +1,20 @@
+//
+//  detailsViewController.swift
+//  SimpsonBook
+//
+//  Created by Birkan Pusa on 3.12.2021.
+//
+
+import UIKit
+
+class detailsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+
+}
